a1eb4dce75be6669046c77047b6e9dd305f05c68
Ashwin <Ashwinsuresh2013@gmail.com>
2023-07-27 19:09:51 +0100

    Adding The SQL Files

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO OWNERS (NAME) VALUES ('ASH'), ('MONIKA'), ('AKSHAT'), ('LINCOLN');
+
+INSERT INTO DOGS (NAME, AGE, OWNER_ID) VALUES
+('Cricket', 10, 1),
+('Rocky', 2, 2),
+('Coco', 3, 3),
+('Romeo', 5, 4);
---
+++ java-api/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE OWNERS(
+owner_id int  AUTO_INCREMENT PRIMARY KEY,
+name varchar(50) not null
+);
+
+CREATE TABLE DOGS(
+ID int  AUTO_INCREMENT PRIMARY KEY,
+name varchar(50) not null,
+age int not null,
+owner_id int not null,
+FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
+);
