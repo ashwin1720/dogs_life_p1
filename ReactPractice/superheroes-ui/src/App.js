ecbd03980919755cd044456ae1b078d8c004e96a
unknown <abharg22@asu.edu>
2023-08-01 15:34:43 +0100

    React

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -1,29 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import HeroDetail from './components/HeroDetail';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
 
-    <HeroDetail/>
+    <AllHeroes/>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
+ 
 }
+
 
 export default App;
